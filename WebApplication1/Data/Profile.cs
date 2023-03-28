a2355c8931f959201935585e30d87729c4536017
Phong <tranvanphong.forwork@gmail.com>
2023-03-28 20:22:49 +0700

    This is speacial feature

---
+++ WebApplication1/Data/Profile.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Data
+{
+    public class Profile
+    {
+        public int Id { get; set; }
+        public string Key { get; set; }
+        public string value { get; set; }
+
+    }
+}
